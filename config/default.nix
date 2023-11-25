b4334c991f005687f001ed05844292720d634d0d
shnCanos <catellacatellacarettacaretta@gmail.com>
2023-11-25 16:24:43 +0000

    Removed old file

--- config/default.nix
+++ config/default.nix
@@ -1,1 +1,39 @@
-{ self, ... }: { imports = [ ./plugins ./mappings ./nixvim.nix ]; }
+{ pkgs, ... }: { 
+  imports = [ ./plugins ./mappings ]; 
+
+  config = {
+    colorschemes.catppuccin = {
+      enable = true;
+      flavour = "mocha";
+      terminalColors = true; # hmm
+      transparentBackground = true;
+    };
+
+    options = {
+      number = true;
+      relativenumber = true;
+      hlsearch = true;
+      mouse = "a"; # Config agnostic because I can disable the touchpad
+      breakindent = true; # Not too sure! 👍
+      undofile = true; # Save undo information
+      ignorecase = true;
+      smartcase = true;
+      termguicolors = true; # Better colors
+    };
+
+    clipboard = {
+      register = "unnamedplus";
+      providers = {
+        wl-copy.enable = true;
+        xclip.enable = true;
+      };
+    };
+
+    globals = {
+      mapleader = " ";
+      maplocalleader = " ";
+    };
+
+    extraPlugins = with pkgs.vimPlugins; [ lazygit-nvim ];
+  };
+}
