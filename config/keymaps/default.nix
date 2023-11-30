7720496e5bf6408d9339c1b337fa8f79e1c0cdfc
shnCanos <catellacatellacarettacaretta@gmail.com>
2023-11-30 08:18:59 +0000

    added haskell for some reason

--- config/keymaps/default.nix
+++ config/keymaps/default.nix
@@ -1,4 +1,37 @@
 { ... }:
+# let
+#   importKeymapsFile = name: import (builtins.toPath (name + ".nix")) { };
+#   joinKeymaps = list:
+#     builtins.foldl' (keys1: keys2: keys1 ++ keys2) (map importKeymapsFile list);
+# in joinKeymaps [
+#   "buffer"
+#   "fileManagers"
+#   "git"
+#   "lsp"
+#   "movement"
+#   "other"
+#   "project"
+#   "terminal"
+#   "window"
+# ]
+
+# let
+#   importKeymapsFile = name: import (./. + name + ".nix") { };
+#   joinKeymaps = list:
+#     builtins.foldl' (keys1: keys2: keys1 ++ keys2) [ ]
+#     (map importKeymapsFile list);
+#
+# in joinKeymaps [
+#   "buffer"
+#   "fileManagers"
+#   "git"
+#   "lsp"
+#   "movement"
+#   "other"
+#   "project"
+#   "terminal"
+#   "window"
+# ]
 
 import ./buffer.nix { } ++ import ./fileManagers.nix { } ++ import ./git.nix { }
 ++ import ./lsp.nix { } ++ import ./movement.nix { } ++ import ./other.nix { }
